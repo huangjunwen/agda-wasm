034da2f5c088115b0227a71d1d048d4905cab057
jayven <kassarar@gmail.com>
2023-08-13 20:06:15 +0800

    Extra setup

--- build/2.6.3/build.sh
+++ build/2.6.3/build.sh
@@ -18,9 +18,26 @@ patch_src () {
   popd
 }
 
+# Build
+
 patch_src Agda-2.6.3
 patch_src vector-hashtables-0.1.1.3
 patch_src zlib-0.6.3.0
 
-# wasm32-wasi-cabal build all --constraint='zlib +bundled-c-zlib' --disable-optimization --ghc-options=-debug
-wasm32-wasi-cabal build all --constraint='zlib +bundled-c-zlib'
+wasm32-wasi-cabal build all
+
+# Extra setup
+
+touch /xyz.agda-lib
+mkdir -p /usr/local/ghc-wasm/.cabal/share/wasm32-wasi-ghc-9.6.2.20230523/Agda-2.6.3
+cp -r /root/build/2.6.3/Agda-2.6.3/src/data/lib/ /usr/local/ghc-wasm/.cabal/share/wasm32-wasi-ghc-9.6.2.20230523/Agda-2.6.3
+
+cat <<EOT > /tmp/test.agda
+module test where   
+    
+data N : Set where   
+  O : N   
+  S : N -> N  
+EOT
+
+wasmtime run --dir=/ /root/build/2.6.3/dist-newstyle/build/wasm32-wasi/ghc-9.6.2.20230523/Agda-2.6.3/build/agda/agda.wasm
